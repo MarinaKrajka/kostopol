3a39b2b6dc23685dd1dbc307a41205edbb1d7289
MarinaKrajka <96741649+MarinaKrajka@users.noreply.github.com>
2022-02-03 13:25:43 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,10 @@
+﻿const hamburger_menu = document.querySelector(".hamburger-menu");
+const container = document.querySelector(".container");
+const content = document.querySelector(".content");
+
+hamburger_menu.addEventListener("click", () => {
+  container.classList.toggle("active");
+    content.classList.toggle("active");
+
+});
+
