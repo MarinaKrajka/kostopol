9be3d627c1a2e676701874d8ddb9b4d71a0a3c0e
MarinaKrajka <96741649+MarinaKrajka@users.noreply.github.com>
2022-02-03 13:09:36 +0200

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-﻿const hamburger_menu = document.querySelector(".hamburger-menu");
-const container = document.querySelector(".container");
-
-hamburger_menu.addEventListener("click", () => {
-  container.classList.toggle("active");
-});
-
