de4f01a29bc5f8f8b420e376b9b2341e84fc0b99
MarinaKrajka <96741649+MarinaKrajka@users.noreply.github.com>
2022-02-05 23:16:24 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 ﻿const hamburger_menu = document.querySelector(".hamburger-menu");
 const container = document.querySelector(".container");
 const content = document.querySelector(".content");
+const mobil = window.matchMedia("screen and (max-width: 649px)");
+                              
 
 hamburger_menu.addEventListener("click", () => {
   container.classList.toggle("active");
     content.classList.toggle("active");
+  if (mobile.matches) {
+    content.classList.toggle("mob");
+  }
 
 });
 
