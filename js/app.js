d916bded2ba6aab49bd2d52dd2b7283f19cf354e
MarinaKrajka <96741649+MarinaKrajka@users.noreply.github.com>
2022-02-07 17:42:37 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 const hamburger_menu = document.querySelector(".hamburger-menu");
 const container = document.querySelector(".container");
+const gerbActive = document.querySelector(".gerb");
 
                               
 
 hamburger_menu.addEventListener("click", () => {
   container.classList.toggle("active");
+  gerbActive.classList.toggle("active");
+  
 });
 
