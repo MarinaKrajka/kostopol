871f65b65160978f3896e9f9202211e240650177
MarinaKrajka <96741649+MarinaKrajka@users.noreply.github.com>
2022-02-16 14:00:03 +0200

    Add files via upload

---
+++ js/h3apear.js
@@ -0,0 +1,17 @@
+function onEntry(entry) {
+	entry.forEach(change => {
+	  if (change.isIntersecting) {
+	   change.target.classList.add('show');
+	  }
+	});
+  }
+  
+  let options = {
+	threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.whoare');
+  
+  for (let elm of elements) {
+	observer.observe(elm);
+  }
+console.log(elements);
